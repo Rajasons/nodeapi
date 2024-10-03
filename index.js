a41bd0b2445f5079874257a7668bed1b155a8f1c
Rajasons <rajasonsrajsonsit@gmail.com>
2024-10-03 14:16:25 +0530

    satish

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const ProductRoute = require("./routes/product_routes");
 const CartRoute = require("./routes/cart_routes");
 const bodeparser = require('body-parser');
 const app = express();
-
+const http = require('http');
+const server = http.createServer(app);
 app.use(express.json());
 app.use(bodeparser.json());
  
@@ -22,7 +23,7 @@ app.use("/api/cart",CartRoute);
 
 mongoose.connect("mongodb+srv://rajsonsit:OPInynsinxsj9ePC@backend.f0fz4.mongodb.net/?retryWrites=true&w=majority&appName=backend").then(()=>{
     console.log("connected");
-    app.listen(3000,()=>{
+    server.listen(3000,()=>{
         console.log("server run on port 3000");
     });
 }).catch((err)=>{
